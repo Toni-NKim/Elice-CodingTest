59779de73f87cade270678619dbfa0f837174695
Toni N Kim <steamin0924@gmail.com>
2021-10-19 07:55:27 +0900

    add explanations to help solve the problem

--- Data Structure/josephus.py
+++ Data Structure/josephus.py
@@ -9,11 +9,16 @@ result = []
 for i in range(1, n + 1):
     dq.append(i)
 
+# deque.rotate() rotates to the right which means that rotate(k) will produce [5, 6, 7, 1, 2, 3, 4]
+# To rotate to the left, put in a negative value.
+# To pop from the left, has to rotate -k + 1
 while len(dq) > 0:
     dq.rotate(-k + 1)
+    print(dq)
     result.append(dq.popleft())
 
 # set output according to the given example (<3, 6, 2, 7, 5, 1, 4>)
+# join cannot iterate through int instances, has to convert to string
 answer = ", ".join(map(str, result))
 print(f"<{answer}>")
 
